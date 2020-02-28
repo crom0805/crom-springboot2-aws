52ad69a4130db3b59911fff77a30f7ae4cc8d7d7
crom0805 <crom0805@gmail.com>
2020-02-29 01:27:30 +0900

    JPA Test

---
+++ src/test/java/crom/accel/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,64 @@
+package crom.accel.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+  @SpringBootTest
+  별다른 설정입이 @SpringBootTest를 사용할 경우 H2데이터베이스를 자동으로 실행해줌
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    /*
+      @After
+      JUnit에서 단위테스트가 끝날때마다 수행되는 메소드를 지정
+      보통은 배포 전 전체 테스트를 수행할 때 테스트간 데이터 침범을 막기 위해 사용
+     */
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        /*
+          postsRepository.save
+          테이블 posts에 insert/update 쿼리를 실행. ID값이 있다면 update, 없다면 insert
+         */
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("crom0805@gmail.com")
+                .build());
+
+        /*
+          postsRepository.findAll
+          테이블 posts에 있는 모든 데이터를 조회해오는 메소드
+         */
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
