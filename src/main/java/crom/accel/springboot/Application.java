fc645e49cbdfb3fa0d9b4d504f2d776e1e041218
crom0805 <crom0805@gmail.com>
2020-03-02 22:14:16 +0900

    JPA Test

--- src/main/java/crom/accel/springboot/Application.java
+++ src/main/java/crom/accel/springboot/Application.java
@@ -2,12 +2,14 @@ package crom.accel.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /*
   @SpringBootApplication
   스프링부트의 자동설정, 스프링Bean 읽기와 생성을 모두 자동으로 생성
   @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야만 함.
  */
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
