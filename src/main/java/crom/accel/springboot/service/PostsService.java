be2b16152b17f5077ca2fc152fb4569126e5dd0c
crom0805 <crom0805@gmail.com>
2020-05-14 22:32:11 +0900

    수정 삭제

--- src/main/java/crom/accel/springboot/service/PostsService.java
+++ src/main/java/crom/accel/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package crom.accel.springboot.service;
 
 import crom.accel.springboot.domain.posts.Posts;
 import crom.accel.springboot.domain.posts.PostsRepository;
+import crom.accel.springboot.web.dto.PostsListResponseDto;
 import crom.accel.springboot.web.dto.PostsResponseDto;
 import crom.accel.springboot.web.dto.PostsSaveRequestDto;
 import crom.accel.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +35,16 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
